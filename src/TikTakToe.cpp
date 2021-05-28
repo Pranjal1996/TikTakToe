5e9cb4677d80fa40349f48f2f3eb09fde12b55e4
Sacred Kai <sacredlegend1209@gmail.com>
2021-05-28 18:15:09 +0530

    Added Draw Function and a line after winning

--- src/TikTakToe.cpp
+++ src/TikTakToe.cpp
@@ -1,4 +1,5 @@
 
+
 #include "Block.h"
 #include<iostream>
 #include<graphics.h>
@@ -10,243 +11,255 @@ int y = 0;
 int player = 0;
 bool validTurn = false;
 
-class PlayButton{
-	int x1;
-	int x2;
-	int y1;
-	int y2;
+class PlayButton {
+int x1;
+int x2;
+int y1;
+int y2;
 
 public:
 
-	//Setting the values for rectangle
-	void SetButton(int a, int b, int c, int d){
-		x1 = a;
-		x2 = b;
-		y1 = c;
-		y2 = d;
-	}
+//Setting the values for rectangle
+void SetButton(int a, int b, int c, int d) {
+x1 = a;
+x2 = b;
+y1 = c;
+y2 = d;
+}
 
-	//Printing from the given values
-	void PrintButton(){
-			setcolor(WHITE);
-			rectangle(x1, y1, x2, y2);
-			}
+//Printing from the given values
+void PrintButton() {
+setcolor(WHITE);
+rectangle(x1, y1, x2, y2);
+}
 
-	//If Play Button is clicked
-	bool ClickButton(int a, int b){
-			if (a >= x1 && a <= x2 && b >= y1 && b <= y2){
-			return true;
-			}
-			else{
-		return false;
-			}
-			}
+//If Play Button is clicked
+bool ClickButton(int a, int b) {
+if (a >= x1 && a <= x2 && b >= y1 && b <= y2) {
+return true;
+}
+else {
+return false;
+}
+}
 
 };
 
-bool TurnTeller(int a){
+bool TurnTeller(int a) {
 
-	//If Kaata is played
-	if (a == true){
-		delay(50);
-		settextstyle(4, 4, 1);
-		outtextxy(360, 135, "Gola's   Turn");
-		return true;
-	}
+//If Kaata is played
+if (a == true) {
+delay(50);
+settextstyle(4, 4, 1);
+outtextxy(360, 135, "Gola's   Turn");
+return true;
+}
 
-	//If Gola is played
-	else{
-		delay(50);
-		settextstyle(4, 4, 1);
-		outtextxy(360, 135, "Kaata's Turn");
-		return false;
-	}
+//If Gola is played
+else {
+delay(50);
+settextstyle(4, 4, 1);
+outtextxy(360, 135, "Kaata's Turn");
+return false;
+}
 }
 
 //Function for printing TicTacToe box, telling the turn and breaking at winning
-void PlayGame(){
+void PlayGame() {
 
-	settextstyle(4, 4, 1);
-	outtextxy(360, 135, "Kaata's Turn");
-	Block a[9];
+settextstyle(4, 4, 1);
+outtextxy(360, 135, "Kaata's Turn");
+Block a[9];
 
-	//Setting the values for blocks
-	a[0].SetValues(200, 250, 200, 250);
-	a[1].SetValues(250, 300, 200, 250);
-	a[2].SetValues(300, 350, 200, 250);
-	a[3].SetValues(200, 250, 250, 300);
-	a[4].SetValues(250, 300, 250, 300);
-	a[5].SetValues(300, 350, 250, 300);
-	a[6].SetValues(200, 250, 300, 350);
-	a[7].SetValues(250, 300, 300, 350);
-	a[8].SetValues(300, 350, 300, 350);
+//Setting the values for blocks
+a[0].SetValues(200, 250, 200, 250);
+a[1].SetValues(250, 300, 200, 250);
+a[2].SetValues(300, 350, 200, 250);
+a[3].SetValues(200, 250, 250, 300);
+a[4].SetValues(250, 300, 250, 300);
+a[5].SetValues(300, 350, 250, 300);
+a[6].SetValues(200, 250, 300, 350);
+a[7].SetValues(250, 300, 300, 350);
+a[8].SetValues(300, 350, 300, 350);
 
-	//Printing the blocks
-	for(int j = 0; j< 9;j++){
-	a[j].PrintBlock();
-	}
+//Printing the blocks
+for (int j = 0; j < 9; j++) {
+a[j].PrintBlock();
+}
 
-	//Infinite loop
-	while(true){
+//Infinite loop
+while (true) {
 
-	while (!ismouseclick(WM_LBUTTONDOWN)){
-	delay(500);
-	}
+while (!ismouseclick(WM_LBUTTONDOWN)) {
+delay(500);
+}
 
-	getmouseclick(WM_LBUTTONDOWN,x,y);
-	//cout << "x is "<<x<<endl;
-	//cout << "y is "<<y<<endl;
+getmouseclick(WM_LBUTTONDOWN, x, y);
+//cout << "x is "<<x<<endl;
+//cout << "y is "<<y<<endl;
 
-	validTurn = false;
+validTurn = false;
 
-	//If block is clicked
-	for(int j = 0; j< 9;j++){
-	if (a[j].BlockClick(x, y)){
+//If block is clicked
+for (int j = 0; j < 9; j++) {
+if (a[j].BlockClick(x, y)) {
 
-	//Take action. In other words, print Kaata or Gola
-	validTurn = a[j].TakeAction(player);
+//Take action. In other words, print Kaata or Gola
+validTurn = a[j].TakeAction(player);
 
-	}
-	}
+}
+}
 
 //Gola Winning
 
-	//Left Vertical
-	if(a[0].GetState() == 1 && a[3].GetState() == 1 && a[6].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		setcolor(CYAN);
-		line(225, 200, 225, 350);
-		break;
-	}
+//Left Vertical
+if (a[0].GetState() == 1 && a[3].GetState() == 1 && a[6].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(225, 200, 225, 350);
+break;
+}
 
-	//Middle Vertical
-	else if(a[1].GetState() == 1 && a[4].GetState() == 1 && a[7].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		setcolor(CYAN);
-		line(275, 200, 275, 350);
-		break;
-	}
+//Middle Vertical
+else if (a[1].GetState() == 1 && a[4].GetState() == 1 && a[7].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(275, 200, 275, 350);
+break;
+}
 
-	//Right vertical
-	else if(a[2].GetState() == 1 && a[5].GetState() == 1 && a[8].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		setcolor(CYAN);
-		line(325, 200, 325, 350);
-		break;
-	}
+//Right vertical
+else if (a[2].GetState() == 1 && a[5].GetState() == 1 && a[8].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(325, 200, 325, 350);
+break;
+}
 
-	//Top left to bottom right diagonal
-	else if(a[0].GetState() == 1 && a[4].GetState() == 1 && a[8].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		setcolor(CYAN);
-		line(200, 200, 350, 350);
-		break;
-	}
+//Top left to bottom right diagonal
+else if (a[0].GetState() == 1 && a[4].GetState() == 1 && a[8].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(200, 200, 350, 350);
+break;
+}
 
-	//Top right to bottom left diagonal
-	else if(a[2].GetState() == 1 && a[4].GetState() == 1 && a[6].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		setcolor(CYAN);
-		line(350, 200, 200, 350);
-		break;
-	}
+//Top right to bottom left diagonal
+else if (a[2].GetState() == 1 && a[4].GetState() == 1 && a[6].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(350, 200, 200, 350);
+break;
+}
 
-	//Top horizontal
-	else if(a[0].GetState() == 1 && a[1].GetState() == 1 && a[2].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		break;
-	}
+//Top horizontal
+else if (a[0].GetState() == 1 && a[1].GetState() == 1 && a[2].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(200, 225, 350, 225);
+break;
+}
 
-	//Middle horizontal
-	else if(a[3].GetState() == 1 && a[4].GetState() == 1 && a[5].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		break;
-	}
+//Middle horizontal
+else if (a[3].GetState() == 1 && a[4].GetState() == 1 && a[5].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(200, 275, 350, 275);
+break;
+}
 
-	//Down horizontal
-	else if(a[6].GetState() == 1 && a[7].GetState() == 1 && a[8].GetState() == 1){
-		outtextxy(360, 135, "Gola Wins!!!!");
-		break;
-	}
+//Down horizontal
+else if (a[6].GetState() == 1 && a[7].GetState() == 1 && a[8].GetState() == 1) {
+outtextxy(360, 135, "Gola Wins!!!!");
+setcolor(CYAN);
+line(200, 325, 350, 325);
+break;
+}
 
-	//Kaata Winning
+//Kaata Winning
 
-		//Left Vertical
-		if(a[0].GetState() == 2 && a[3].GetState() == 2 && a[6].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			setcolor(CYAN);
-			line(225, 200, 225, 350);
-			break;
-		}
+//Left Vertical
+if (a[0].GetState() == 2 && a[3].GetState() == 2 && a[6].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(225, 200, 225, 350);
+break;
+}
 
-		//Middle Vertical
-		else if(a[1].GetState() == 2 && a[4].GetState() == 2 && a[7].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			setcolor(CYAN);
-			line(275, 200, 275, 350);
-			break;
-		}
+//Middle Vertical
+else if (a[1].GetState() == 2 && a[4].GetState() == 2 && a[7].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(275, 200, 275, 350);
+break;
+}
 
-		//Right vertical
-		else if(a[2].GetState() == 2 && a[5].GetState() == 2 && a[8].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			setcolor(CYAN);
-			line(325, 200, 325, 350);
-			break;
-		}
+//Right vertical
+else if (a[2].GetState() == 2 && a[5].GetState() == 2 && a[8].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(325, 200, 325, 350);
+break;
+}
 
-		//Top left to bottom right diagonal
-		else if(a[0].GetState() == 2 && a[4].GetState() == 2 && a[8].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			setcolor(CYAN);
-			line(200, 200, 350, 350);
-			break;
-		}
+//Top left to bottom right diagonal
+else if (a[0].GetState() == 2 && a[4].GetState() == 2 && a[8].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(200, 200, 350, 350);
+break;
+}
 
-		//Top right to bottom left diagonal
-		else if(a[2].GetState() == 2 && a[4].GetState() == 2 && a[6].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			setcolor(CYAN);
-			line(350, 200, 200, 350);
-			break;
-		}
+//Top right to bottom left diagonal
+else if (a[2].GetState() == 2 && a[4].GetState() == 2 && a[6].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(350, 200, 200, 350);
+break;
+}
 
-		//Top horizontal
-		else if(a[0].GetState() == 2 && a[1].GetState() == 2 && a[2].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			break;
-		}
+//Top horizontal
+else if (a[0].GetState() == 2 && a[1].GetState() == 2 && a[2].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(200, 225, 350, 225);
+break;
+}
 
-		//Middle horizontal
-		else if(a[3].GetState() == 2 && a[4].GetState() == 2 && a[5].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			break;
-		}
+//Middle horizontal
+else if (a[3].GetState() == 2 && a[4].GetState() == 2 && a[5].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(200, 275, 350, 275);
+break;
+}
 
-		//Down horizontal
-		else if(a[6].GetState() == 2 && a[7].GetState() == 2 && a[8].GetState() == 2){
-			outtextxy(360, 135, "Kaata Wins!!!!");
-			break;
-		}
+//Down horizontal
+else if (a[6].GetState() == 2 && a[7].GetState() == 2 && a[8].GetState() == 2) {
+outtextxy(360, 135, "Kaata Wins!!!!");
+setcolor(CYAN);
+line(200, 325, 350, 325);
+break;
+}
 
-		//Draw
-		else if(a[0].GetOccuState() == true && a[1].GetOccuState() == true && a[2].GetOccuState() == true && a[3].GetOccuState() == true && a[4].GetOccuState() == true && a[5].GetOccuState() == true && a[6].GetOccuState() == true && a[7].GetOccuState() == true && a[8].GetOccuState() == true){
-			outtextxy(360, 135, "!!!!!!Draw!!!!!!");
-			break;
-		}
+//Draw
+else if (a[0].GetOccuState() == true && a[1].GetOccuState() == true && a[2].GetOccuState() == true && a[3].GetOccuState() == true && a[4].GetOccuState() == true && a[5].GetOccuState() == true && a[6].GetOccuState() == true && a[7].GetOccuState() == true && a[8].GetOccuState() == true) {
+outtextxy(360, 135, "!!!!!!Draw!!!!!!");
+break;
+}
 
-	if(validTurn){
-	player = !player;
-	}
+if (validTurn) {
+player = !player;
+}
 
-	TurnTeller(player);
+TurnTeller(player);
 
-	}
+}
 
 
 }
 
-int main(){
+int main() {
 
 int gd = DETECT, gm;
 initgraph(&gd, &gm, NULL);
@@ -269,15 +282,15 @@ outtextxy(630, 470, "Designed by The Narayan Brothers");
 settextstyle(10, 4, 1);
 outtextxy(480, 30, "Welcome To The KaataGola Game!!!");
 
-while (!ismouseclick(WM_LBUTTONDOWN)){
+while (!ismouseclick(WM_LBUTTONDOWN)) {
 delay(500);
 
-getmouseclick(WM_LBUTTONDOWN,x,y);
+getmouseclick(WM_LBUTTONDOWN, x, y);
 //cout << "x is "<<x<<endl;
 //cout << "y is "<<y<<endl;
 
 
-if(Play.ClickButton(x, y)){
+if (Play.ClickButton(x, y)) {
 PlayGame();
 }
 }
